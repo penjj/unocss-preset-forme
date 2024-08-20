ab001b3d30bc8c3908d951db7486a50a9be233cd
penjj <peng.j.1995@gmail.com>
2024-08-20 10:09:09 +0800

    fix: fix esm

--- unocss-preset-forme/src/utils.ts
+++ unocss-preset-forme/src/utils.ts
@@ -1,9 +1,8 @@
 import { readFile } from 'node:fs/promises'
-import { resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { createRequire } from 'node:module'
 import type { ResetCSS } from './types'
 
-const __dirname = fileURLToPath(import.meta.url)
+const { resolve } = createRequire(import.meta.url)
 
 function hyphenate(str: string) {
   return str.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`)
@@ -30,8 +29,5 @@ export async function resolveResetCSS(type: ResetCSS): Promise<string> {
 }
 
 export function resolveCSSText(type: string) {
-  return readFile(
-    resolve(__dirname, `../node_modules/@unocss/reset/${type}.css`),
-    'utf8',
-  )
+  return readFile(resolve(`@unocss/reset/${type}.css`), 'utf8')
 }
